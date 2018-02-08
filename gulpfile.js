8ececb3b0d8b9d3b51ea38a980294842e672d8fa
memo2318 <guillermo.carapia@icomsys.com.mx>
2018-02-08 13:16:02 -0600

    proyecto base

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,46 @@
-const elixir = require('laravel-elixir');
+let gulp = require('gulp');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let sass = require('gulp-sass');
+let rename = require("gulp-rename");
+let browserSync = require('browser-sync').create();
+let plumber = require('gulp-plumber');
 
-require('laravel-elixir-vue-2');
+gulp.task('default', ['uglify', 'scss', 'watch']);
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application as well as publishing vendor resources.
- |
- */
+gulp.task('uglify', () => {
+  return gulp.src('./resources/assets/js/**/*.js')
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('./public/js'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/js'))
+});
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+gulp.task('scss', () => {
+  return gulp.src('./resources/assets/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(plumber())
+        .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('js/**/*.js', ['uglify']);
+    gulp.watch('scss/**/*.scss', ['scss']);
+});
+
+gulp.task('serve', ['scss'], function() {
+    browserSync.init({
+      proxy: 'crea.test',
+      files: [
+          './resources/views/**/*.blade.php',
+      ]
+    });
+    gulp.watch('./resources/assets/sass/**/*.scss', ['scss', 'browserSync:reload']);
+    gulp.watch('./resources/assets/js/**/*.js', ['uglify', 'browserSync:reload']);
+ });
+
+gulp.task('browserSync:reload', () => {
+   browserSync.reload();
 });
